2fe6162cb2f2a665fb0d105b5ff74412add09a5d
MANPEET <sahejsingh2309@gmail.com>
2026-05-17 19:12:54 -0600

    Updated endpoints

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -9,10 +9,12 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin:["http://localhost:5173"]
+        origin: process.env.NODE_ENV === "production" 
+            ? "https://chat-app-2-tgg1.onrender.com"
+            : "http://localhost:5173",
+        credentials: true
     }
 })
-
 const userSocketMap = {}
 
 export function getReceiverSocketId(userId) {
--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,14 @@ dotenv.config()
 connectDB()
 
 
-
 app.use(
     cors({
-      origin: "http://localhost:5173",
-      credentials: true,
+        origin: process.env.NODE_ENV === "production"
+            ? "https://chat-app-2-tgg1.onrender.com"
+            : "http://localhost:5173",
+        credentials: true,
     })
-);
+);;
 
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ limit: "10mb", extended: true }))
@@ -33,12 +34,12 @@ app.use("/api/auth", router)
 app.use("/api/messages", messageRouter)
 app.use("/api/groups", groupRouter)
 
-if(process.env.NODE_ENV == "Production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 
