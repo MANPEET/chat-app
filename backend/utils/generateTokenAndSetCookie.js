28df5f31071f927ed9e26bf69d021b0ed2c3e543
MANPEET <sahejsingh2309@gmail.com>
2026-05-17 19:43:19 -0600

    Updated cookies to work in cross-origin

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken"
 
-export const generateTokenAndSetCookie = (userId,res) => {
-    const Token = jwt.sign({userId},process.env.JWT_SECRET,{
+export const generateTokenAndSetCookie = (userId, res) => {
+    const Token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: '15d'
     })
 
-    res.cookie("token",Token,{
-        sameSite:"strict",
+    res.cookie("token", Token, {
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         maxAge: 15 * 24 * 60 * 60 * 1000,
-        secure: process.env.NODE_ENV !== "development"
     })
 
     return Token
